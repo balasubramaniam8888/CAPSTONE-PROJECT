abdd90036eed07b21ad42bda73ba3505cdd2d35f
balasubramaniam8888 <144506570+balasubramaniam8888@users.noreply.github.com>
2024-06-14 00:33:15 +0530

    RECENT COMMIT

--- react-markdown-app/frontend/react-markdown-viewer/src/main.jsx
+++ react-markdown-app/frontend/react-markdown-viewer/src/main.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
